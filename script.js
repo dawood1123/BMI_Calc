d94d7b1f038c442f0ef5d6d1de172f43789e4dd2
dawood1123 <davidkhan1226@gmail.com>
2024-10-09 22:46:45 -0700

    comments added to script file

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 function w() { 
+    // Get value of weight when user enter
     var w = document.getElementById('weight_input');
     var w_value = w.value; 
 
+    // Get value of height when user enter
     var h = document.getElementById('height_input');
     var h_value = h.value; 
 
     // Check if either field is empty
     if (w_value === "" || h_value === "") { 
-        alert("Please fill in both fields");
+        alert("Please fill  both fields");
     }
     else { 
-        var calculate = w_value / (h_value * h_value); 
+        var calculate = w_value / (h_value * h_value); // calculating BMI
 
         var bmi = document.getElementById('bmi-value');     
-        bmi.innerHTML = `BMI: ${calculate.toFixed(2)}`; 
+        bmi.innerHTML = `BMI: ${calculate.toFixed(2)}`; // rounding off to two decimal places 
 
         var massage = document.getElementById('message');
 
+        // if statment for massages according to BMI values
         if (calculate < 18.5) { 
             massage.innerHTML = "Underweight";
         }
